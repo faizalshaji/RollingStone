7ea59861e13028070c5bbb900a8dc3bd99583c82
Faizal Shaji <faizalshaji@gmail.com>
2023-01-29 21:47:02 +0530

    Assets and animation

--- RollingStone/Assets/Player.cs
+++ RollingStone/Assets/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public Rigidbody2D Rigidbody;
+    public bool OnGround;
+    public LayerMask GroundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        OnGround = Physics2D.Raycast(transform.position, Vector2.down, 2, GroundLayer);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Rigidbody.AddForce(Vector2.right, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Rigidbody.AddForce(Vector2.left, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.Space) &&  OnGround)
+        {
+            Rigidbody.velocity = Vector2.up * 5;
+        }
     }
 }
